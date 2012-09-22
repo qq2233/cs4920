2d3a9bea58be2e0711c9864a4a781400b0ed947d
mathew <mathewchong7@gmail.com>
2012-09-22 11:49:23 +1000

    Capital letters for table names

--- schema.sql
+++ schema.sql
@@ -1,18 +1,18 @@
-CREATE TABLE libraries (
+CREATE TABLE Libraries (
 	id INTEGER NOT NULL,
 	name TEXT,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE entries (
+CREATE TABLE Entries (
 	id INTEGER NOT NULL,
 	library INTEGER NOT NULL, --Every entry must have a source
 	word TEXT,	--the actual word, eg. teacher
 	PRIMARY KEY (id),
-	FOREIGN KEY (library) REFERENCES libraries(id)
+	FOREIGN KEY (library) REFERENCES Libraries(id)
 );
 
-CREATE TABLE meanings (
+CREATE TABLE Meanings (
 	id INTEGER NOT NULL,
 	entry INTEGER,
 	kana TEXT, --phonetic reading
@@ -20,22 +20,22 @@ CREATE TABLE meanings (
 	morpheneType TEXT, --eg noun, verb, particle
 	definition TEXT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (entry) REFERENCES entries(id)
+	FOREIGN KEY (entry) REFERENCES Entries(id)
 );
 
 --Relationship between entries and meaning
---entries may have many (or none) meanings
-CREATE TABLE entriesHasAMeaning (
+--Entries may have many (or none) meanings
+CREATE TABLE EntriesHasAMeaning (
 	entry INTEGER,
 	meaning INTEGER,
 	PRIMARY KEY (entry, meaning),
-	FOREIGN KEY (entry) REFERENCES entries(id),
-	FOREIGN KEY (meaning) REFERENCES meanings(id)
+	FOREIGN KEY (entry) REFERENCES Entries(id),
+	FOREIGN KEY (meaning) REFERENCES Meanings(id)
 );
 
 --A usage example is either a sentence or phrase
 --set phrase NULL if it's a sentence, and vice versa
-CREATE TABLE usageExamples (
+CREATE TABLE UsageExamples (
 	id INTEGER NOT NULL,
 	expression TEXT,
 	meaning TEXT,
@@ -48,26 +48,26 @@ CREATE TABLE usageExamples (
 --A usage example can have many sources, avoiding duplication
 --by having this table.
 --This is the 'part of' relationship between library and usageExample
-CREATE TABLE usageExampleSource (
+CREATE TABLE UsageExampleSource (
 	usageExample INTEGER NOT NULL,
 	library INTEGER NOT NULL,
 	PRIMARY KEY (usageExample, library),
-	FOREIGN KEY (usageExample) REFERENCES usageExamples(id), 
-	FOREIGN KEY (library) REFERENCES libraries(id)
+	FOREIGN KEY (usageExample) REFERENCES UsageExamples(id), 
+	FOREIGN KEY (library) REFERENCES Libraries(id)
 );
 
 --Relationship between usageExample and Entries
-CREATE TABLE usageConsistsOf (
+CREATE TABLE UsageConsistsOf (
 	usageExample INTEGER NOT NULL,
 	entry INTEGER,
 	position INTEGER,
 	wordLength INTEGER,
 	PRIMARY KEY (usageExample),
-	FOREIGN KEY (usageExample) REFERENCES usageExamples(id),
-	FOREIGN KEY (entry) REFERENCES entries(id)
+	FOREIGN KEY (usageExample) REFERENCES UsageExamples(id),
+	FOREIGN KEY (entry) REFERENCES Entries(id)
 );
 	
-CREATE TABLE lists (
+CREATE TABLE Lists (
 	id INTEGER NOT NULL,
 	name TEXT, 
 	listType TEXT,
@@ -75,7 +75,7 @@ CREATE TABLE lists (
 );
 
 --relationship between usageExample and List
-CREATE TABLE usageApartOfList (
+CREATE TABLE UsageApartOfList (
 	id INTEGER NOT NULL,
 	toLearn TEXT,
 	list INTEGER,
