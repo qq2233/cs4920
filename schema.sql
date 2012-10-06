9b5d1c22e2ea63bd32e2f59a808081059f04c531
mathew <mathewchong7@gmail.com>
2012-10-06 23:28:50 +1000

    tabs->spaces

--- schema.sql
+++ schema.sql
@@ -2,112 +2,112 @@
 --It can include EPWING dictionaries, Anki Decks,
 --Sentence corpuses, etc.
 CREATE TABLE Libraries (
-	id INTEGER NOT NULL,
-	name TEXT NOT NULL UNIQUE,
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     type INTEGER NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (type) REFERENCES LibraryTypes(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (type) REFERENCES LibraryTypes(id)
 );
 
 CREATE TABLE LibraryTypes (
-	id INTEGER NOT NULL,
-	type TEXT NOT NULL UNIQUE,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    type TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Entries (
-	id INTEGER NOT NULL,
+    id INTEGER NOT NULL,
     library INTEGER NOT NULL,
     number INTEGER,
     kana INTEGER NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (library) REFERENCES Libraries(id),
-	FOREIGN KEY (kana) REFERENCES Morphemes(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (library) REFERENCES Libraries(id),
+    FOREIGN KEY (kana) REFERENCES Morphemes(id)
 );
 
 CREATE TABLE EntryHasKanji (
-	entry INTEGER NOT NULL,
-	kanji INTEGER NOT NULL,
-	PRIMARY KEY (entry, kanji),
-	FOREIGN KEY (entry) REFERENCES Entries(id),
-	FOREIGN KEY (kanji) REFERENCES Morphemes(id)
+    entry INTEGER NOT NULL,
+    kanji INTEGER NOT NULL,
+    PRIMARY KEY (entry, kanji),
+    FOREIGN KEY (entry) REFERENCES Entries(id),
+    FOREIGN KEY (kanji) REFERENCES Morphemes(id)
 );
 
 CREATE TABLE Meanings (
-	id INTEGER NOT NULL,
-	meaning TEXT,
-	entry INTEGER NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (entry) REFERENCES Entries(id)
+    id INTEGER NOT NULL,
+    meaning TEXT,
+    entry INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (entry) REFERENCES Entries(id)
 );
 
 --A usage example is either a sentence or phrase
 --set isSentence 1 if it's a sentence, 0 if phrase
 CREATE TABLE UsageExamples (
-	id INTEGER NOT NULL,
-	expression TEXT NOT NULL,
-	meaning TEXT,
-	reading TEXT,
-	isSentence INTEGER NOT NULL,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    expression TEXT NOT NULL,
+    meaning TEXT,
+    reading TEXT,
+    isSentence INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE MeaningHasUEs (
-	usageExample INTEGER NOT NULL,
+    usageExample INTEGER NOT NULL,
     meaning INTEGER NOT NULL,
-	PRIMARY KEY (usageExample, meaning),
-	FOREIGN KEY (meaning) REFERENCES Meanings(id),
-	FOREIGN KEY (usageExample) REFERENCES usageExamples(id)
+    PRIMARY KEY (usageExample, meaning),
+    FOREIGN KEY (meaning) REFERENCES Meanings(id),
+    FOREIGN KEY (usageExample) REFERENCES usageExamples(id)
 );
 
 --Fill with all possible types of morphemes
 CREATE TABLE MorphemeTypes (
-	id INTEGER NOT NULL,
-	type TEXT NOT NULL UNIQUE,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    type TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Morphemes (
-	id INTEGER NOT NULL,
-	morpheme TEXT NOT NULL,
-	morphemeType INTEGER, --might make not null in future
+    id INTEGER NOT NULL,
+    morpheme TEXT NOT NULL,
+    morphemeType INTEGER, --might make not null in future
     count INTEGER, --num occurrances in known list (anki deck)
     UNIQUE (morpheme, morphemeType),
-	PRIMARY KEY (id),
-	FOREIGN KEY (morphemeType) REFERENCES morphemeTypes(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (morphemeType) REFERENCES morphemeTypes(id)
 );
 
 CREATE TABLE UEConsistsOf (
-	usageExample INTEGER NOT NULL,
-	morpheme INTEGER NOT NULL,
-	wordLength INTEGER, --maybe make this & pos not null
-	position INTEGER,
-	PRIMARY KEY (usageExample, morpheme),
-	FOREIGN KEY (usageExample) REFERENCES UsageExamples(id),
-	FOREIGN KEY (morpheme) REFERENCES Morphemes(id)
+    usageExample INTEGER NOT NULL,
+    morpheme INTEGER NOT NULL,
+    wordLength INTEGER, --maybe make this & pos not null
+    position INTEGER,
+    PRIMARY KEY (usageExample, morpheme),
+    FOREIGN KEY (usageExample) REFERENCES UsageExamples(id),
+    FOREIGN KEY (morpheme) REFERENCES Morphemes(id)
 );
 
 --User and System created lists of UEs
 CREATE TABLE Lists (
-	id INTEGER NOT NULL,
-	name TEXT NOT NULL, 
-	listType INTEGER NOT NULL, --might change this in the future
-	PRIMARY KEY (id),
-	FOREIGN KEY (listType) REFERENCES ListTypes(id)
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL, 
+    listType INTEGER NOT NULL, --might change this in the future
+    PRIMARY KEY (id),
+    FOREIGN KEY (listType) REFERENCES ListTypes(id)
 );
 
 CREATE TABLE ListTypes (
-	id INTEGER NOT NULL,
-	type TEXT, 
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    type TEXT, 
+    PRIMARY KEY (id)
 );
 
 --relationship between usageExample and List
 CREATE TABLE UEPartOfList (
-	list INTEGER NOT NULL,
-	usageExample INTEGER NOT NULL,
-	toLearn TEXT, --may link to morpheme in future
-	PRIMARY KEY (list, usageExample),
-	FOREIGN KEY (usageExample) REFERENCES UsageExamples(id),
-	FOREIGN KEY (list) REFERENCES Lists(id)
+    list INTEGER NOT NULL,
+    usageExample INTEGER NOT NULL,
+    toLearn TEXT, --may link to morpheme in future
+    PRIMARY KEY (list, usageExample),
+    FOREIGN KEY (usageExample) REFERENCES UsageExamples(id),
+    FOREIGN KEY (list) REFERENCES Lists(id)
 );
