eec2647729a1963bc80a241fc125940c7c1e0b28
mathew <mathewchong7@gmail.com>
2012-10-06 16:29:17 +1000

    basic importfile->python working, waiting for db interface and DAOs

--- noj/ue_library_importer.py
+++ noj/ue_library_importer.py
@@ -6,6 +6,11 @@ import codecs
 
 re_header_line = re.compile(r'(\w+?): ?(.*)$')
 re_entry = re.compile(r'^[^\t]')
+re_entry_split = re.compile(r'^([^\t]*)\t([^\t]*)\t([^\t]*)\n')
+re_meaning = re.compile(r'^\t[^\t]')
+re_meaning_split = re.compile(r'^\t([^\t]*)\n')
+re_ue = re.compile(r'^\t\t[^\t]')
+re_ue_split = re.compile(r'^\t\t([^\t]*)\t([^\t]*)\n')
 
 SENTENCE = 0
 PHRASE = 1
@@ -26,9 +31,21 @@ class DictionaryEntry(object):
         """docstring for add_meaning"""
         self.meanings.append(meaning)
 
+    def num_meanings(self):
+        """docstring for num_meanings"""
+        return len(self.meanings)
+
     def __str__(self):
-        return ("{} {} [{}]".format(
-            self.kana, self.entry_number, self.kanji))  
+        meaning_str_list = list()
+        for m in self.meanings:
+            meaning_str_list.extend(str(m).split('\n'))
+        meaning_str = '\n'.join(["\t{}".format(m) 
+                                 for m in meaning_str_list])
+        kanji_str = u'・'.join(self.kanji)
+        return ("{} {} [{}]\n{}".format(self.kana.encode('utf-8'), 
+                                    self.entry_number, 
+                                    kanji_str.encode('utf-8'),
+                                    meaning_str)) 
 
 
 class DictionaryMeaning(object):
@@ -43,6 +60,16 @@ class DictionaryMeaning(object):
         """docstring for add_ue"""
         self.usage_examples.append(ue)
 
+    def __str__(self):
+        ue_str_list = list()
+        for ue in self.usage_examples:
+            ue_str_list.extend(str(ue).split('\n'))
+        ue_str = '\n'.join(["\t{}".format(ue) for ue in ue_str_list])
+        return ("{}: {}\n{}".format(self.meaning_number, 
+                                self.meaning.encode('utf-8'),
+                                ue_str))
+
+
 class UsageExample(object):
     """docstring for UsageExample"""
     def __init__(self, expression, meaning, type_=SENTENCE):
@@ -50,6 +77,10 @@ class UsageExample(object):
         self.expression = expression
         self.meaning = meaning
         self.type_ = type_
+
+    def __str__(self):
+        return ("{}\n{}".format(self.expression.encode('utf-8'),
+                                self.meaning.encode('utf-8')))
 
 class UELibraryImporter(object):
     """Imports usage example libraries"""
@@ -86,21 +117,48 @@ class UELibraryImporter(object):
 
     def read_entry(self):
         """docstring for read_entry"""
-        in_entry = False
+        lines = list()
         for line in self.fh:
-            print self.prev_line,
-            if re_entry.match(self.prev_line):
-                in_entry == True
+            #print self.prev_line,
+            lines.append(self.prev_line)
             self.prev_line = line
             if re_entry.match(line):
                 break
+        return self.parse_entry_lines(lines)
+
+    def parse_entry_lines(self, lines):
+        """docstring for parse_entry_lines"""
+        entry = None
+        for line in lines:
+            if re_entry.match(line):
+                #print "entry", line,
+                m = re_entry_split.match(line)
+                entry = DictionaryEntry(m.group(1), m.group(2).split('|'), 
+                                        m.group(3))
+            elif re_meaning.match(line):
+                #print "meaning", line,
+                m = re_meaning_split.match(line)
+                meaning_no = entry.num_meanings()+1
+                meaning = DictionaryMeaning(m.group(1), meaning_no)
+                entry.add_meaning(meaning)
+                #print meaning
+            elif re_ue.match(line):
+                #print "ue", line,
+                m = re_ue_split.match(line)
+                ue = UsageExample(m.group(1), m.group(2))
+                meaning.add_usage_example(ue)
+                #print ue
+        print entry
+        return entry
 
 def main():
     importer = UELibraryImporter('out2')
     lib_type = importer.type_
-    importer.read_entry()
-    importer.read_entry()
-    importer.read_entry()
+    entry = True
+    while entry is not None:
+        entry = importer.read_entry()
+    #importer.read_entry()
+    #importer.read_entry()
     #if lib_type == DICTIONARY:
         #while importer.not_end_of_file():
             #entry = importer.read_entry()
